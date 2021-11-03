7ea30053f3a226d5ba983663eb10e076f05b6fa5
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-11-02 22:54:03 -0400

    add file

--- hw5-3.py
+++ hw5-3.py
@@ -1,4 +1,16 @@
 #author CJP 11/2/2021
 
 import calendar
+#Question 1
 calendar.TextCalendar().pryear(2020)
+# Question 2
+calendar.TextCalendar(6).pryear(2020)
+# Question 3
+calendar.TextCalendar().prmonth(2021, 6)
+
+# Question 4
+# N/A doesnt work
+
+# Question 5
+print(calendar.isleap(2021))
+# It expect a year for the argument, as a result it returns false which is a boolean value.
